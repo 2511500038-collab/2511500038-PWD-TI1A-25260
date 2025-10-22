1624ac7c695f9bc45a9115c9d05e003c20d0253e
Randy orlando <2511500038@atmaluhur.ac.id>
2025-10-22 12:33:27 +0700

    onclik listener tombol hamburger

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,4 @@
 document.getElementById("menutoggle").addEventListener("click", function() {
     document.querySelector("nav").classList.toggle("active");
+    console.log("yoimya is the best");
 });
